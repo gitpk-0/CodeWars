8b1429f16c1f4d34e78b2e159435778800610b30
gitpk-0 <kell108p@protonmail.com>
2023-03-30 10:48:20 -0400

    maximum subarray sum java 5 kyu

--- 5_kyu/Java/MaxSubarraySum/maxSubarraySum/src/main/Max.java
+++ 5_kyu/Java/MaxSubarraySum/maxSubarraySum/src/main/Max.java
@@ -11,28 +11,40 @@ public class Max {
     // Max.sequence(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
     // should be 6: {4, -1, 2, 1}
 
+    // Max.sequence(new int[]{8,-29,0,-28,-20,9,15,-15,-8,8,-6,-14,-13,-1,14,21,-10,-29,3,7,-9,-23,-28,26,-28,12,-4,-1,27,18,10,21,27,-8,-5,-17,28,-21,-8,-10,1,-16,-18,-3,-11,24,-28,-7,28,-2,13});
+
 
     public static int sequence(int[] arr) {
-        if (Arrays.stream(arr).sum() < 0) {
-            return 0;
-        }
+        int maxSubarrSum = 0;
+        int length = 1;
+        int startIndex = 0;
 
-        ArrayList<Integer> sumArr = new ArrayList<>();
-        ArrayList<Integer> indexTracker = new ArrayList<>();
+        while (startIndex < arr.length) {
+            ArrayList<Integer> arr2 = new ArrayList<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            sumArr.add(arr[i]);
-            int first = sumArr.stream().mapToInt(n -> n).sum();
-            int second = first + arr[i + 1];
-            if (first < second) {
-                sumArr.add(arr[i + 1]);
+            while (length <= arr.length) {
+                for (int i = startIndex; i < length; i++) {
+                    arr2.add(arr[i]);
+
+                    int currentSum = arr2.stream().mapToInt(j -> j).sum();
+
+                    if (currentSum > maxSubarrSum) {
+                        maxSubarrSum = currentSum;
+                    }
+
+                }
+                length++;
+                arr2 = new ArrayList<>();
             }
+            length = 0;
+            startIndex++;
         }
-        return 10101010;
+
+        return maxSubarrSum;
     }
 
     public static void main(String[] args) {
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] arr = {-3, -20, -6, -27, 25, -20, 29, -4, 21, 10, 7, -8, 5, 11, -1, 19, 20, -1, 2, -5, -10, 14, -19, -11, 17, -28, 24, -14, 19, -25, 27, -18, -15, -20, 14, 17, 28, -23, -23, 5, -11, -17, 12, 5, 10, -2, 24, 3, -13, -8, 23, -3, -24, 29, -2, -18, -28, -18, 19};
         System.out.println(sequence(arr));
     }
 }
