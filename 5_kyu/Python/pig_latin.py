4ca0d14a247619fd6af073ed07690397f1842a34
gitpk-0 <kell108p@protonmail.com>
2023-04-03 07:55:11 -0400

    simple pig latin python 5 kyu

---
+++ 5_kyu/Python/pig_latin.py
@@ -0,0 +1,18 @@
+def pig_it(text):
+    arr = text.split(" ")
+    out = ""
+    for i in arr:
+        if i[0].isalpha():
+            out += i[1::] + i[0] + "ay "
+        else:
+            out += i
+    if out[len(out) - 1] == " ":
+        out = out[:-1]
+    return out
+
+
+
+print(pig_it('Pig latin is cool')) # igPay atinlay siay oolcay
+print(pig_it('Hello world !'))     # elloHay orldway
+
+print(pig_it('O tempora o mores !')) # Oay emporatay oay oresmay !
