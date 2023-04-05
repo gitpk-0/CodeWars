e8df1132f54be05a5fd68f7c5ba4a379cce1f74e
gitpk-0 <kell108p@protonmail.com>
2023-04-04 20:18:41 -0400

    weight for weight python 5 kyu

--- 5_kyu/Python/weight_for_weight.py
+++ 5_kyu/Python/weight_for_weight.py
@@ -1,27 +1,35 @@
 def order_weight(strng):
-    lst = strng.split(" ")
-    this_dict = {}
+    if strng == "":
+        return ""
+    weights = strng.split(" ")
+    w8s_dict = {}
 
-    for num in lst:
-        digits = list(num)
+    for weight in weights:
+        digits = list(weight)
         total = 0
         for digit in digits:
             total += int(digit)
-        this_dict[num] = total
 
-    this_dict = sorted(this_dict.items(), key=lambda x:x[1])
-    this_dict = dict(this_dict)
+        try:
+            w8s_dict[total].append(int(weight))
+            w8s_dict[total] = sorted(w8s_dict[total], key=str) # sort the list alphabetically (integers)
+        except:
+            w8s_dict[total] = [int(weight)]
+
+    w8s_dict = dict(sorted(w8s_dict.items())) # sort the dict keys
 
     output = ""
-    for k in this_dict.keys():
-        output += f"{k} "
+
+    for v in w8s_dict.values():
+        for i in v:
+            output += f"{str(i)} "
         
-    return output[:-1]
+    return output[:-1] # or output.strip()
             
 
 
 
-# print(order_weight("103 123 4444 99 2000")) # 2000 103 123 4444 99
+print(order_weight("103 123 4444 99 2000")) # 2000 103 123 4444 99
 print(order_weight("2000 10003 1234000 44444444 9999 11 11 22 123")) 
 # 11 11 2000 10003 22 123 1234000 44444444 9999
-# print(order_weight("")) # 
+print(order_weight("")) # 
