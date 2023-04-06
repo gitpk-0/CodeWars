d28338607470567a29cb505823afc941e3c0bee6
gitpk-0 <kell108p@protonmail.com>
2023-04-06 11:41:01 -0400

    human readable duration format (with debug) python 4 kyu

--- 4_kyu/Python/human_readable_duration.py
+++ 4_kyu/Python/human_readable_duration.py
@@ -24,69 +24,89 @@ def format_duration(seconds):
         seconds_remaining = "0" + str(seconds_remaining)
 
     result = f"{years}:{days}:{hours}:{minutes}:{seconds_remaining}"
-    print(result)
+    # print(result)
 
     parts = result.split(":")
     # print(parts[0])
 
     output = ""
-    # count = 0
+    length = 0
+    count = 0
+    timeframe = ["year", "day", "hour", "minute", "second"]
+    for i in range(0, len(parts)):
+        if parts[i] != "00":
+            length += 1
 
-    if parts[0] != "00":
-        # count += 1
-        years = int(parts[0])
-        if years > 1:
-            output += str(years) + " years"
-        else:
-            output += str(years) + " year"
+    # print(f"length: {length}")
 
-    if parts[1] != "00":
-        count += 1
-        if output != "":
-            if parts[2] == "00" and parts[3] == "00" and parts[4] == "00":
-                output += " and "
+    for i in range(0, len(parts)):
+        # print(parts[i])
+        if parts[i] != "00":
+            time = int(parts[i])
+            if output != "":
+                if count == length -1:
+                    output +=" and "
+                else:
+                    output +=", "
+
+            if time > 1:
+                output += str(time) + f" {timeframe[i]}s"
+                count += 1
             else:
-                output += ", "
-        days = int(parts[1])
-        if days > 1:
-            output += str(days) + " days"
-        else:
-            output += str(days) + " day"
+                output += str(time) + f" {timeframe[i]}"
+                count += 1
 
-    if parts[2] != "00":
-        count += 1
-        if output != "":
-            if count > 1 and parts[3] == "00" and parts[4] == "00":
-                output += " and "
-            else:
-                output += ", "
-        hours = int(parts[2])
-        if hours > 1:
-            output += str(hours) + " hours"
-        else:
-            output += str(hours) + " hour"
+    # if parts[0] != "00":
+    #     years = int(parts[0])
+    #     if years > 1:
+    #         output += str(years) + " years"
+    #     else:
+    #         output += str(years) + " year"
+
+    # if parts[1] != "00":
+    #     if output != "":
+    #         if parts[2] == "00" and parts[3] == "00" and parts[4] == "00":
+    #             output += " and "
+    #         else:
+    #             output += ", "
+    #     days = int(parts[1])
+    #     if days > 1:
+    #         output += str(days) + " days"
+    #     else:
+    #         output += str(days) + " day"
+
+    # if parts[2] != "00":
+    #     if output != "":
+    #         if parts[3] == "00" and parts[4] == "00":
+    #             output += " and "
+    #         else:
+    #             output += ", "
+    #     hours = int(parts[2])
+    #     if hours > 1:
+    #         output += str(hours) + " hours"
+    #     else:
+    #         output += str(hours) + " hour"
     
-    if parts[3] != "00":
-        count += 1
-        if output != "":
-            if count > 1 and parts[4] == "00":
-                output += " and "
-            else:
-                output += ", "
-        minutes = int(parts[3])
-        if minutes > 1:
-            output += str(minutes) + " minutes"
-        else:
-            output += str(minutes) + " minute"
+    # if parts[3] != "00":
+    #     if output != "":
+    #         if parts[4] == "00":
+    #             output += " and "
+    #         else:
+    #             output += ", "
+    #     minutes = int(parts[3])
+    #     if minutes > 1:
+    #         output += str(minutes) + " minutes"
+    #     else:
+    #         output += str(minutes) + " minute"
 
-    if parts[4] != "00":
-        if output != "":
-            output += " and "
-        seconds = int(parts[4])
-        if seconds > 1:
-            output += str(seconds) + " seconds"
-        else:
-            output += str(seconds) + " second"
+    # if parts[4] != "00":
+    #     if output != "":
+    #         output += " and "
+    #     seconds = int(parts[4])
+    #     if seconds > 1:
+    #         output += str(seconds) + " seconds"
+    #     else:
+    #         output += str(seconds) + " second"
 
     return output
 
@@ -100,7 +120,7 @@ def format_duration(seconds):
 # print(format_duration(120)) # "2 minutes"
 # print(format_duration(3600)) # "1 hour" 3301980
 # print(format_duration(3662)) # "1 hour, 1 minute and 2 seconds"  
-# print(format_duration(15731080)) # "182 days, 1 hour, 44 minutes and 40 seconds"
-# print(format_duration(132030240)) # "4 years, 68 days, 3 hours and 4 minutes"
-# print(format_duration(15731080)) # "182 days, 1 hour, 44 minutes and 40 seconds"
+print(format_duration(15731080)) # "182 days, 1 hour, 44 minutes and 40 seconds"
+print(format_duration(132030240)) # "4 years, 68 days, 3 hours and 4 minutes"
+print(format_duration(15731080)) # "182 days, 1 hour, 44 minutes and 40 seconds"
 print(format_duration(3301980)) # "38 days, 5 hours and 13 minutes"
