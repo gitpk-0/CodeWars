310024acf94c0685f143f728d0f33d0a5fd50290
gitpk-0 <kell108p@protonmail.com>
2023-04-08 10:52:52 -0400

    directions reduction (still trying) java 5 kyu

--- 5_kyu/Java/DirectionsReduction/dirReduc/src/main/DirReduction.java
+++ 5_kyu/Java/DirectionsReduction/dirReduc/src/main/DirReduction.java
@@ -1,90 +1,70 @@
 package main;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 
 public class DirReduction {
 
     public static String[] dirReduc(String[] arr) {
-        for (String i : arr) {
-            System.out.print(i);
-            System.out.print(", ");
-        }
-
-        HashMap<String, Integer> dirs = new HashMap<>();
+        ArrayList<String> dirs = new ArrayList<>();
         ArrayList<String> output = new ArrayList<>();
-
         for (String dir : arr) {
-            if (output.size() == 0) {
+            dirs.add(dir);
+        }
+        System.out.println("dirs: " + dirs.toString());
+
+        int index = 0;
+        for (String dir : dirs) {
+            if (dir.equals("NORTH") && !dirs.get(index + 1).equals("SOUTH")) {
                 output.add(dir);
-                continue;
+            }
+            if (dir.equals("SOUTH") && !dirs.get(index + 1).equals("NORTH")) {
+                output.add(dir);
+            }
+            if (dir.equals("EAST") && !dirs.get(index + 1).equals("WEST")) {
+                output.add(dir);
+            }
+            if (dir.equals("WEST") && !dirs.get(index + 1).equals("EAST")) {
+                output.add(dir);
             }
 
-            int countDir = Collections.frequency(output, dir);
-
-            if (dir.equals("SOUTH") && )
-
-
-
-
-
-
-
-
-
-            if (dirs.containsKey(dir)) {
-                int add = dirs.get(dir) + 1;
-                dirs.put(dir, add);
-            } else {
-                dirs.put(dir, 1);
-            }
         }
+        System.out.println("out: " + output.toString());
 
-        // north south comparison
-        int n_s_diff = dirs.get("NORTH") - dirs.get("SOUTH");
-        int e_w_diff = dirs.get("EAST") - dirs.get("WEST");
-
-        if (e_w_diff > 0) {
-            while (e_w_diff > 0) {
-                output.add("EAST");
-                e_w_diff--;
-            }
-        }
-
-        if (e_w_diff < 0) {
-            while (e_w_diff < 0) {
-                output.add("WEST");
-                e_w_diff++;
-            }
-        }
-
-        if (n_s_diff > 0) {
-            while (n_s_diff > 0) {
-                output.add("NORTH");
-                n_s_diff--;
-            }
-        }
-
-        if (n_s_diff < 0) {
-            while (n_s_diff < 0) {
-                output.add("SOUTH");
-                n_s_diff++;
-            }
-        }
+        // if (index > 0) {
+        //     if (dir.equals("NORTH") && dirs.get(index - 1).equals("SOUTH")) {
+        //         dirs.remove(index);
+        //         dirs.remove(index - 1);
+        //     }
+        //
+        //     if (dir.equals("SOUTH") && dirs.get(index - 1).equals("NORTH")) {
+        //         dirs.remove(index);
+        //         dirs.remove(index - 1);
+        //     }
+        //
+        //     if (dir.equals("EAST") && dirs.get(index - 1).equals("WEST")) {
+        //         dirs.remove(index);
+        //         dirs.remove(index - 1);
+        //     }
+        //
+        //     if (dir.equals("WEST") && dirs.get(index - 1).equals("EAST")) {
+        //         dirs.remove(index);
+        //         dirs.remove(index - 1);
+        //     }
+        // }
 
 
-
-
-        System.out.println(output.toString());
-
+        // HashMap<String, ArrayList<Integer>> dirs = new HashMap<>();
+        // ArrayList<String> output = new ArrayList<>();
+        // System.out.println(output.toString());
+        // System.out.println(key.toString() + ": " + dirs.get(key).toString());
         return output.toArray(new String[]{});
+        // return new String[]{};
     }
+
 
     public static void main(String[] args) {
         System.out.println(dirReduc(new String[]{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"})); // WEST
 
-        System.out.println(dirReduc(new String[]{"EAST", "EAST", "WEST", "NORTH", "WEST", "EAST", "EAST", "SOUTH", "NORTH", "WEST"})); // EAST
+        System.out.println(dirReduc(new String[]{"EAST", "EAST", "WEST", "NORTH", "WEST", "EAST", "EAST", "SOUTH", "NORTH", "WEST"})); // EAST, NORTH
     }
 }
