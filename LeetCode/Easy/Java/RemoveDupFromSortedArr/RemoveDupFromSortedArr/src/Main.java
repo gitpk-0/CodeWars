b21e5e9c7c1ec797ded30b0d86536c6a6a869ce2
gitpk-0 <kell108p@protonmail.com>
2023-04-08 20:36:58 -0400

    remove dup from sort arr easy java

--- LeetCode/Easy/Java/RemoveDupFromSortedArr/RemoveDupFromSortedArr/src/Main.java
+++ LeetCode/Easy/Java/RemoveDupFromSortedArr/RemoveDupFromSortedArr/src/Main.java
@@ -1,26 +1,20 @@
-import java.util.ArrayList;
 
 public class Main {
 
     public static int removeDuplicates(int[] nums) {
-        ArrayList<Integer> numsA = new ArrayList<>();
-        int count_unique = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (!(numsA.contains(nums[i]))) {
-                numsA.add(nums[i]);
-
-                if (nums[count_unique] != nums[i]) {
-                    nums[count_unique] = nums[i];
-                }
+        int count_unique = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] != nums[i]) {
+                nums[count_unique] = nums[i];
                 count_unique++;
             }
         }
-        return numsA.size();
+        return count_unique;
     }
 
     public static void main(String[] args) {
         // int[] nums = new int[]{0,0,1,1,1,2,2,3,3,4};
-        int[] nums = new int[]{1,1,2};
+        int[] nums = new int[]{1, 1, 2};
         System.out.println(Main.removeDuplicates(nums));
     }
 }
