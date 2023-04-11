3578af12160086afe871d6194d9cebcbbbadf0ae
gitpk-0 <kell108p@protonmail.com>
2023-04-11 11:29:37 -0400

    convert binary num to an int java easy

---
+++ LeetCode/Easy/Java/ConvertBinNumToInt/ConvertBinNumToInt/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public int getDecimalValue(ListNode head) {
+        ListNode temp = head;
+        int sum = 0;
+        int count = 0;
+        while (temp != null) {
+            try {
+                temp = head.next;
+                count++;
+            } catch (Exception e) {
+                break;
+            }
+        }
+
+        while (head != null) {
+            sum += (head.val * ((int) Math.pow(2, count)));
+            try {
+                head = head.next;
+                count--;
+            } catch (Exception e) {
+                break;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- LeetCode/Easy/Java/RotateArray/RotateArray/src/Main.java
+++ LeetCode/Easy/Java/RotateArray/RotateArray/src/Main.java
@@ -1,68 +1,93 @@
-
+import java.util.Arrays;
 
 public class Main {
 
     public static int[] rotate(int[] nums, int k) {
-        // copy len - k - 1 (mid), paste to len -1 (end)
-        // copy len - 1 (end), paste to index 0
-        // copy index 0, paste to len - k - 1 (mid)
-        int midInd = nums.length - k - 1;
-        int endInd = nums.length - 1;
-        int strtInd = 0;
-
-        int midVal = nums[midInd];
-        int endVal = nums[endInd];
-        int strtVal = nums[strtInd];
-
-        nums[endInd] = midVal;
-        nums[strtInd] = endVal;
-        nums[midInd] = strtVal;
-
-        for (int i : nums) {
-            System.out.print(i+",");
+        if (nums.length <= 1) {
+            // return;
+        }
+        if (nums.length == 2) {
+            while (k > 0) {
+                int temp = nums[0];
+                nums[0] = nums[1];
+                nums[1] = temp;
+                k--;
+            }
         }
 
-        midInd = nums.length - k - 1;
-        midInd++;
-        endInd = nums.length - 1;
-        endInd--;
-        strtInd = 0;
-        strtInd++;
-
-        midVal = nums[midInd];
-        endVal = nums[endInd];
-        strtVal = nums[strtInd];
-
-        nums[endInd] = midVal;
-        nums[strtInd] = endVal;
-        nums[midInd] = strtVal;
-
-
-        System.out.println();
-        for (int i : nums) {
-            System.out.print(i+",");
+        if (nums.length == 3) {
+            while (k > 0) {
+                int temp = nums[0];
+                int temp2 = nums[1];
+                nums[0] = nums[2];
+                nums[1] = temp;
+                nums[2] = temp2;
+                k--;
+            }
         }
+        if (nums.length % 2 != 0) { // odd length array
+            int kcopy = k;
+            int midInd = nums.length - k - 1;
+            int endInd = nums.length - 1;
+            int strtInd = 0;
+            int midVal, endVal, strtVal;
 
-        midInd = nums.length - k - 1;
-        // endInd = nums.length - 1; // stays the same on last iteration
-        strtInd++;
+            while (kcopy > 0) {
+                if (kcopy > 1) {
+                    if (kcopy != k) {
+                        midInd++;
+                        endInd--;
+                        strtInd++;
+                    }
+                    midVal = nums[midInd];
+                    endVal = nums[endInd];
+                    strtVal = nums[strtInd];
 
-        // last iteration swap strtInd and endInd
-        endVal = nums[endInd];
-        strtVal = nums[strtInd];
+                    nums[endInd] = midVal;
+                    nums[strtInd] = endVal;
+                    nums[midInd] = strtVal;
+                }
+                // last iteration
+                if (kcopy == 1) {
+                    strtInd++;
+                    endVal = nums[endInd];
+                    strtVal = nums[strtInd];
 
-        nums[endInd] = strtVal;
-        nums[strtInd] = endVal;
+                    nums[endInd] = strtVal;
+                    nums[strtInd] = endVal;
+                }
+                kcopy--;
+            }
 
-        System.out.println();
-        for (int i : nums) {
-            System.out.print(i+",");
+
+            // reverse indexes 0 thru k
+            for (int i = 0; i < k / 2; i++) {
+                int temp = nums[i];
+                nums[i] = nums[k - i - 1];
+                nums[k - i - 1] = temp;
+            }
+        } else { // even length array
+            // last index to k index
+            // k index to last
+            // last index - 1 to k - 1
+            // k -1 index to last - 1
+
+            int last = nums.length - 1;
+            while (k > 0) {
+                int temp = nums[last];
+                nums[last] = nums[k - 1];
+                nums[k - 1] = temp;
+                k--;
+                last--;
+            }
         }
 
         return nums;
     }
 
     public static void main(String[] args) {
-        System.out.println(rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3));
+        // System.out.println(rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3));
+        System.out.println(Arrays.toString(rotate(new int[]{-1, -100, 3, 99}, 2)));
+        System.out.println(Arrays.toString(rotate(new int[]{-1, -100, 3, 99}, 3)));
     }
 }
