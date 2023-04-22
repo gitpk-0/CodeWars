eba8b72b76583c0dce2cbbb77b0eeeb940dcb023
gitpk-0 <kell108p@protonmail.com>
2023-04-22 11:11:48 -0400

    generate hashtag (start) python 5 kyu

---
+++ 4_kyu/Python/next_bigger.py
@@ -0,0 +1,16 @@
+def next_bigger(n):
+
+    if n <= 9:
+        return -1
+    return n
+
+
+
+
+ 
+print(next_bigger(  12)) #   21
+# print(next_bigger(  21)) #   -1
+# print(next_bigger( 513)) #  531
+# print(next_bigger(2017)) # 2071
+# print(next_bigger( 414)) #  441
+# print(next_bigger( 144)) #  414
---
+++ 5_kyu/Python/generate_hashtag.py
@@ -0,0 +1,26 @@
+def generate_hashtag(s):
+    return
+
+
+
+
+print(generate_hashtag('Codewars')) 
+# '#Codewars', 'Should handle a single word.')
+print(generate_hashtag('Codewars      ')) 
+# '#Codewars', 'Should handle trailing whitespace.')
+print(generate_hashtag('      Codewars')) 
+# '#Codewars', 'Should handle leading whitespace.')
+print(generate_hashtag('Codewars Is Nice')) 
+# '#CodewarsIsNice', 'Should remove spaces.')
+print(generate_hashtag('codewars is nice')) 
+# '#CodewarsIsNice', 'Should capitalize first letters of words.')
+print(generate_hashtag('CoDeWaRs is niCe')) 
+# '#CodewarsIsNice', 'Only the first letter of each word should be capitalized in the final hashtag, all other letters must be lower case.')
+print(generate_hashtag('c i n')) 
+# '#CIN', 'A single letter is considered to be a word of length 2, so should capitalize first letters of words of length 1.')
+print(generate_hashtag('codewars  is  nice')) 
+# '#CodewarsIsNice', 'Should deal with unnecessary middle spaces.')
+print(generate_hashtag('')) 
+# False, 'Expected an empty string to return False')
+print(generate_hashtag('Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat')) 
+# False, 'Should return False if the final string is longer than 140 chars.')
